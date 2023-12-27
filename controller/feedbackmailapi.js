d3a55f7128d1d9d459b5b5568dde93a1d514ce59
Shadab khan <techshadab04@gmail.com>
2023-12-27 15:03:04 +0530

    Update feedbackmailapi.js

--- controller/feedbackmailapi.js
+++ controller/feedbackmailapi.js
@@ -4,7 +4,7 @@ async function sendFeedbackMail(name, email, feedback) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "sakshitelang691@gmail.com",
+      user: "demo747724@gmail.com",
       pass: process.env.SENDER_MAIL_PASSWORD,
     },
   });
